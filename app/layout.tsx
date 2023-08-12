5f99efdd1b17205964f76b1028ce72b80bbefc71
prateekvarma <prtk.varma@gmail.com>
2023-08-11 21:45:06 -0600

    Created sidebar, and general changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,25 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Figtree } from 'next/font/google'
+import Sidebar from "@/components/Sidebar";
+import "./globals.css";
+import type { Metadata } from "next";
+import { Figtree } from "next/font/google";
 
-const font = Figtree({ subsets: ['latin'] })
+const font = Figtree({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Spotify Clone',
-  description: 'Listen to music with the Spotify clone app',
-}
+  title: "Spotify Clone",
+  description: "Listen to music with the Spotify clone app",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <Sidebar>{children}</Sidebar>
+      </body>
     </html>
-  )
+  );
 }
